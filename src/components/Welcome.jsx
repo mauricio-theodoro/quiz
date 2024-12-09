0b503452c9fcb772e518484a8ed4e655a1dcb31b
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2024-12-09 20:45:10 -0300

    Adiciona novos estilos em CSS e imagens ao projeto Quiz

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import "../styles/Welcome.css";
+import Quiz from "../img/quiz.svg";
 
 export const Welcome = () => {
   return (
-    <div>
+    <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
         <button>Iniciar</button>
-        <img src="" alt="" />
+        <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
 }
