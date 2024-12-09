f56949616984939a4910a34f7a6780068ba4bcfc
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2024-12-09 20:29:10 -0300

    Adiciona funcionalidades básicas ao projeto Quiz

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Welcome = () => {
+  return (
+    <div>
+        <h2>Seja bem-vindo</h2>
+        <p>Clique no botão abaixo para começar:</p>
+        <button>Iniciar</button>
+        <img src="" alt="" />
+    </div>
+  )
+}
